f51459ba4b558b003931cf7948a1ab78fa34a901
Waltersyd <sydney.onyango@student.moringaschool.com>
2024-07-11 14:35:25 +0300

    Index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// introduction(name)
+function introduction(name){
+    return `Hi, my name is ${name}.`
+}
+
+// introductionWithLanguageOptional(name, language)
+function introductionWithLanguage(name, language){
+return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+//introductionWithLanguageOptional(name, language)
+function introductionWithLanguageOptional(name, language="JavaScript"){
+return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
